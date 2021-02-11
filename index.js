39ddfd0fa49ef262ca8a5e869c519523766aeb5b
okorkov <alex.okorkov@gmail.com>
2021-02-11 14:46:01 -0800

    commit tree 3337cfb8070167d94d329328e269991c75911a90

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const driver = {name: 'Alex', age: '27'}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const new_driver = Object.assign({}, driver)
+  new_driver[key] = value
+  return new_driver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+  const new_driver = Object.assign({}, driver);
+  delete new_driver[key];
+  return new_driver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
+}
